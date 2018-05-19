c3bac2ac754e2fc1b5e244084e60646ea9f40eba
Doro <call.me.dorrro@gmail.com>
2018-05-19 15:17:35 +0200

    Zadanie02.1

--- 02Konstruktory/01Czolg/Czolg.cs
+++ 02Konstruktory/01Czolg/Czolg.cs
@@ -1,10 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _01Czolg {
+    class Samochod : ICloneable
+    {
+        public Samochod(double longitude, double latitude, string marka, string model, double pojenoscSilnika)
+        {
+            Lokalizacja = new GeoLocalization { Latitude = latitude, Longitude = longitude };
+            Marka = marka;
+            Model = model;
+            Silnik = new Silnik(pojenoscSilnika);
+        }
+
+        public Samochod(GeoLocalization lokalizacja, string marka, string model, double pojenoscSilnika)
+        {
+            Lokalizacja = lokalizacja;
+            Marka = marka;
+            Model = model;
+            Silnik = new Silnik(pojenoscSilnika);
+        }
+
+        public Samochod(GeoLocalization lokalizacja, string marka, string model, Silnik silnik)
+        {
+            Lokalizacja = lokalizacja;
+            Marka = marka;
+            Model = model;
+            Silnik = silnik;
+        }
+
+        public Samochod(GeoLocalization lokalizacja, string marka, string model) : this(lokalizacja,marka,model, new Silnik(1.4))
+        { }
+
+        public Samochod(string marka, string model, Silnik silnik) : this(new GeoLocalization { Latitude = 0, Longitude = 0}, marka, model, silnik)
+        { }
+
+        public Samochod(string marka, string model) : this(marka, model, new Silnik(1.4))
+        { }
+
+        public GeoLocalization Lokalizacja { get; }
+
+        public string Marka { get; }
+
+        public string Model { get; }
+
+        public Silnik Silnik { get; }
+
+        public object Clone()
+        {
+            return new Samochod((GeoLocalization) Lokalizacja.Clone(), Marka, Model, (Silnik) Silnik.Clone());
+        }
+    }
+
+    internal class Silnik : ICloneable
+    {
+        public double Pojemnosc { get; private set; }
+
+        public Silnik(double pojemnosc)
+        {
+            Pojemnosc = pojemnosc;
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+
+    internal struct GeoLocalization : ICloneable
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+
     class Czolg {
         private int numerCzolgu;
         private string nazwa;
@@ -53,7 +128,7 @@ namespace _01Czolg {
             nazwa = nowaNazwa;
         }
         public Czolg Klonuj() {
-            return (Czolg)this.MemberwiseClone();
+            return (Czolg)MemberwiseClone();
         }
         public Czolg(Czolg prototyp) {
             numerCzolgu = prototyp.numerCzolgu;
