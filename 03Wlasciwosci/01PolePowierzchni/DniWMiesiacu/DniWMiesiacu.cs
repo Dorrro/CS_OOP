07e0e388dccac410af2df7ab47b8d0230bcadcbb
Doro <call.me.dorrro@gmail.com>
2018-05-19 15:29:02 +0200

    Zadanie03.2

---
+++ 03Wlasciwosci/01PolePowierzchni/DniWMiesiacu/DniWMiesiacu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DniWMiesiacu
+{
+    public class DniWMiesiacu
+    {
+        private readonly int[] _iloscDni = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        private readonly string[] _nazwyMiesiecy = {
+            "styczen", "luty", "marzec", "kwiecien", "maj", "lipiec", "sierpien", "wrzesien", "pazdziernik", "listopad",
+            "grudzien"
+        };
+
+        public int this[string nazwaMiesiaca]
+        {
+            get
+            {
+                var index = Array.IndexOf(_nazwyMiesiecy, nazwaMiesiaca);
+                return _iloscDni[index];
+            }
+            set
+            {
+                this[nazwaMiesiaca] = value;
+            }
+        }
+    }
+}
