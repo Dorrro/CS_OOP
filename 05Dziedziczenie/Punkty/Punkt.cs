e369ed131721b2431819ee4512bf2756d9549262
Doro <call.me.dorrro@gmail.com>
2018-05-19 15:46:36 +0200

    Zadanie05.1

---
+++ 05Dziedziczenie/Punkty/Punkt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Punkty
+{
+    public class Punkt
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public double OdlegloscDoPunktu(Punkt p)
+        {
+            return Math.Sqrt(Math.Pow(p.X - X, 2) + Math.Pow(p.Y - Y, 2));
+        }
+    }
+
+    public class Punkt3D : Punkt
+    {
+        public double Z { get; set; }
+
+        public double OdlegloscDoPunktu(Punkt3D p)
+        {
+            return Math.Sqrt(Math.Pow(this.OdlegloscDoPunktu((Punkt) p), 2) + Math.Pow(p.Z - Z, 2));
+        }
+    }
+}
